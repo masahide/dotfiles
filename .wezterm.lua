556a1e425ce09034fa75ebcb5f6c196c9cccab25
YAMASAKI Masahide <masahide.y@gmail.com>
2023-01-25 00:11:45 +0900

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,26 @@
 local wezterm = require 'wezterm';
 
+function isModuleAvailable(name)
+  if package.loaded[name] then
+    return true
+  else
+    for _, searcher in ipairs(package.searchers or package.loaders) do
+      local loader = searcher(name)
+      if type(loader) == 'function' then
+        package.preload[name] = loader
+        return true
+      end
+    end
+    return false
+  end
+end
+
+local unix_domains = {}
+if isModuleAvailable('weztermlocal') then
+    local weztermlocal = require 'weztermlocal'
+    unix_domains = weztermlocal.unix_domains
+end
+
 if wezterm.target_triple:find("windows") then
     default_prog = {'wsl.exe', '~', '-d', 'Ubuntu-20.04'}
 end
@@ -9,5 +30,6 @@ if wezterm.target_triple:find("darwin") then
 end
 
 return {
-      default_prog = default_prog,
+    default_prog = default_prog,
+    unix_domains = unix_domains,
 }

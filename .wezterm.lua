a4debcc3cf37f2e49d23a70274a3813517d69d55
YAMASAKI Masahide <masahide.y@gmail.com>
2023-01-25 20:11:03 +0900

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -32,4 +32,13 @@ end
 return {
     default_prog = default_prog,
     unix_domains = unix_domains,
+    visual_bell = {
+        fade_in_function = 'EaseIn',
+        fade_in_duration_ms = 150,
+        fade_out_function = 'EaseOut',
+        fade_out_duration_ms = 150,
+    },
+    colors = {
+        visual_bell = '#202020',
+    },
 }
